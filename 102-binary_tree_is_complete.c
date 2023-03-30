f5fdd5ca006e182b61f2271a2b34fc2ad89390c5
MissJuly <joygitau98@gmail.com>
2023-03-30 15:11:01 +0300

    Add 102-binary_tree_is_complete file
    Advanced 3

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -1,48 +1,57 @@
 #include "binary_trees.h"
 
 /**
- * leaf_check - Check if a node is a leaf
- * @node: Node to check
- * Return: 1 if leaf, 0 if not
- **/
-int leaf_check(const binary_tree_t *node)
+ * binary_tree_size - measures the size of a binary tree
+ * @tree: root node from which to measure
+ * Return: total of `tree` and all nodes below it, or 0 if `tree` is NULL
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
 {
-	if (node == NULL)
+	if (!tree)
 		return (0);
-	if (node->left == NULL && node->right == NULL)
+
+	if (!tree->left && !tree->right)
 		return (1);
-	return (0);
+
+	return (1 + binary_tree_size(tree->left) +
+		binary_tree_size(tree->right));
 }
 
 /**
- * binary_tree_is_complete - Check if a tree is complete
- * @tree: The root of the tree to check
- * Return: 1 if tree is complete, 0 if it's not complete
- **/
+ * check_bt_complete - recursively checks if binary tree is complete (nodes
+ * at each level except 2nd from last all have 2 children, level before last
+ * can have less, but all leaves must be as far left as possible)
+ * @tree: root node from which to measure
+ * @i: iterator to mimic an array index while traversing tree
+ * @size: total number of nodes in tree
+ * Return: 1 if `tree` is complete, 0 if not or `tree` is NULL
+ */
+int check_bt_complete(const binary_tree_t *tree, size_t i, size_t size)
+{
+	if (!tree)
+		return (1);
+
+	if ((tree->left && (2 * i) + 1 >= size) ||
+	    !check_bt_complete(tree->left, (2 * i) + 1, size))
+		return (0);
+
+	if ((tree->right && (2 * i) + 2 >= size) ||
+	    !check_bt_complete(tree->right, (2 * i) + 2, size))
+		return (0);
+
+	return (1);
+}
+
+/**
+ * binary_tree_is_complete - calls recursive helper to see if binary tree is
+ * complete
+ * @tree: root of tree or subtree
+ * Return: 1 if `tree` is complete, 0 if not or `tree` is NULL
+ */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
-	binary_tree_t *left_child, *right_child;
-
-	if (tree == NULL)
+	if (!tree)
 		return (0);
-	if (tree->left == NULL && tree->right == NULL)
-		return (1);
-	left_child = tree->left;
-	right_child = tree->right;
-	if (right_child != NULL)
-	{
-		if ((right_child->left != NULL && right_child->right != NULL) &&
-		    (left_child->left == NULL || left_child->right == NULL))
-			return (0);
-	}
-	if (left_child != NULL)
-	{
-		if ((left_child->left != NULL && left_child->right != NULL) &&
-		    (right_child->left == NULL || right_child->right == NULL))
-			return (0);
-	}
-	if (leaf_check(right_child) && leaf_check(left_child))
-		return (1);
-	return (binary_tree_is_complete(left_child) &&
-		binary_tree_is_complete(right_child));
+
+	return (check_bt_complete(tree, 0, binary_tree_size(tree)));
 }
